3cdb86dbdc02fbae95914beb19c8625a043d0304
Ananda Samajdar <anandsamajdar09@gmail.com>
2020-07-19 20:15:29 -0400

    better object image

--- turicreate_example/my_turicreate.py
+++ turicreate_example/my_turicreate.py
@@ -1,8 +1,7 @@
 import turicreate as tc
 
 # Load the starter images
-#starter_img_path = '../medical_data/probe_images/probe_image_resized.png'
-starter_img_path = './images/medical_images/probe_images/probe_image_resized.png'
+starter_img_path = './images/medical_images/probe_images/probe_image.png'
 starter_images = tc.SFrame({'image':[tc.Image(starter_img_path)],
                             'label':['probe']}
                            )
@@ -10,15 +9,12 @@ starter_images = tc.SFrame({'image':[tc.Image(starter_img_path)],
 my_custom_background_images = []
 vid_name = ['Vid1', 'Vid2', 'Vid3', 'Vid4']
 num_img_vid = [192, 213, 169, 152]
-#prefix = '../medical_data/converted_mp4/extracted_frames/'
 prefix = './images/medical_images/converted_mp4/extracted_frames/'
 
 for i in range(len(vid_name)):
-#for i in range(1):
     num_img = num_img_vid[i]
     vid = vid_name[i]
 
-    #for n in range(num_img):
     for n in range(3):
         filename = prefix + vid + '/'
         filename += 'img'
@@ -29,15 +25,10 @@ for i in range(len(vid_name)):
 
         my_custom_background_images.append(image)
 
-#print(my_custom_background_images[0])
-
-#background_images = tc.SFrame({'image':my_custom_background_images})
 background_images = tc.SArray(data=my_custom_background_images)
 
-#background_images.explore()
+model = tc.one_shot_object_detector.create(starter_images, 'label',
+                                           backgrounds=background_images)
 
-#model = tc.one_shot_object_detector.create(starter_images, 'label',
-#                                           backgrounds=background_images)
-
-#model.save('probe_id.model')
+model.save('probe_id_custom_bk.model')
 
--- turicreate_example/turicreate_train.py
+++ turicreate_example/turicreate_train.py
@@ -1,6 +1,6 @@
 import turicreate as tc
 
-starter_img_path = './images/medical_images/probe_images/probe_image_resized.png'
+starter_img_path = './images/medical_images/probe_images/probe_image.png'
 starter_images = tc.SFrame({'image':[tc.Image(starter_img_path)],
                             'label':['probe']}
                            )
